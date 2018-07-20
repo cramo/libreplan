2e7f672a3374d99d0b2dd0939061099923b75fca
draconm01 <draconm01@gmail.com>
2018-07-20 15:15:53 +0200

    2 new page object

---
+++ src/test/java/fr/eql/autom2/Libreplantest/pageobject/FormulairesQualitePage.java
@@ -0,0 +1,12 @@
+package fr.eql.autom2.Libreplantest.pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class FormulairesQualitePage extends MasterPage {
+
+	public FormulairesQualitePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
