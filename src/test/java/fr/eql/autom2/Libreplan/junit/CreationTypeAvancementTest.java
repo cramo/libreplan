22ab91143b0b8e009f524fd80f252cb130090c15
draconm01 <draconm01@gmail.com>
2018-07-24 10:56:44 +0200

    Mise à jour de mes deux Test ainsi qu'une de leurs PageObject

--- src/test/java/fr/eql/autom2/Libreplan/junit/CreationTypeAvancementTest.java
+++ src/test/java/fr/eql/autom2/Libreplan/junit/CreationTypeAvancementTest.java
@@ -4,13 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -18,33 +13,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
 
-import org.dbunit.IDatabaseTester;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.ITable;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import fr.eql.autom2.Libreplantest.pageobject.CompagnyViewPage;
-import fr.eql.autom2.Libreplantest.pageobject.LoginPage;
 import fr.eql.autom2.Libreplantest.pageobject.MasterPage;
 import fr.eql.autom2.Libreplantest.pageobject.TypesDAvancementPage;
 
-public class AVA_01_CreationTypeAvancementTest extends MasterTest {
+public class CreationTypeAvancementTest extends MasterTest {
 
 	Boolean verif;
 	MasterPage menu;
 	TypesDAvancementPage typesDAvancementPage;
 
 	@Test
-	public void CreationTypeAvancementTest() throws FileNotFoundException, IOException {
+	public void monCreationTypeAvancementTest() throws FileNotFoundException, IOException, InterruptedException {
 
 		// TimeOut automatique
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -78,43 +61,61 @@ public class AVA_01_CreationTypeAvancementTest extends MasterTest {
 		assertEquals("vérifie que le bouton est affiché", true, typesDAvancementPage.btnCreer.isDisplayed()); 
 		// clique sur le bouton Créer
 		typesDAvancementPage.clickOnBtnCreer();
-		
+
 		// vérifie le titre
 		assertEquals("vérifie le titre", "Modifier",typesDAvancementPage.lblModifierTitre.getText());
 		// vérifie le Nom d'unité : champ de saisie non renseigné
 		assertEquals("vérifie le champ nom unite","",typesDAvancementPage.txtNomUnite.getText());		
 		// vérifie la Actif : case à cocher cochée par défaut
-		assertEquals("vérifie la case Actif est coché", true,typesDAvancementPage.chkbActif.isSelected());
-		
-		
-		
-		
-	
-		// !!!!!!!!!!
+		assertEquals("vérifie la case Actif est coché", true,typesDAvancementPage.chkbActif.isSelected());	
+
+		// ----- deux vérification non possible à cause du retour null des deux champs ------- //
 		// vérifie la Valeur maximum par défaut : champ de saisie avec pour valeur par défaut "100,00"
-		//System.out.println(typesDAvancementPage.txtValeurMax.getText());
+		//System.out.println(typesDAvancementPage.txtValeurMax.getText() + valeur txtValeurMax); // renvoie null
 		//assertEquals("vérifie la Valeur maximum par défaut","100,00",typesDAvancementPage.txtValeurMax.getText());
-		
 		// vérifie la précision
+		//System.out.println(typesDAvancementPage.txtPrecision.getText() + "valeur txtpecision"); // valeur null
 		//assertEquals("vérifie la précision","Précision",typesDAvancementPage.lblPrecision.getText());
-		// vérifie le type
-		//assertEquals("vérifie le type","Type",typesDAvancementPage.lblType.getText());
-		// vérifie le pourcentage
-		//assertEquals("vérifie le pourcentage","Pourcentage",typesDAvancementPage.lblPourcentage.getText());
+
+		// vérifie Type : Valeur non modifiable "User"
+		try {
+			typesDAvancementPage.lblType.sendKeys("color");
+		}catch(Exception e) {
+			verif = false;
+		}
+		assertEquals("vérifie le type",false,verif);
+		// Pourcentage : case à cocher décochée par défaut
+		assertEquals("vérifie le pourcentage",false,typesDAvancementPage.chkbPourcentage.isSelected());
+		// vérifier les boutons [Enregistrer], [Sauver et continuer] et [Annuler].
+		assertEquals("vérifie la présences du bouton : [Enregistrer]",true,typesDAvancementPage.btnEnregistrer.isDisplayed());
+		assertEquals("vérifie la présences du bouton : [Sauver et continuer]",true,typesDAvancementPage.btnSauverContinuer.isDisplayed());
+		assertEquals("vérifie la présences du bouton : [Annuler]",true,typesDAvancementPage.btnAnnuler.isDisplayed());
 
 
 		// TODO : Etape 4 - Créer un type d'avancement - sans pourcentage :
 		System.out.println("Etape 4 - Créer un type d'avancement - sans pourcentage :");
 		// initialise des variables
-		String nomUnite = "Test 1";
+		String nomUnite = "Type avancement - Test 1";
 		String valeurMax = "10,00";
 		// Remplie les champs Nom Unite et Valeur Max
-		typesDAvancementPage.remplirChampNomUniteValeurMax(nomUnite,valeurMax);
+		typesDAvancementPage.fillFieldNomUniteValeurMax(nomUnite,valeurMax);
 		// Clique sur le bouton enregistrer
-		typesDAvancementPage.clickOnEnregistrer();
+		typesDAvancementPage.clickOnEnregistrer();		
 		
-		
-		
+		// Le message suivant est affiché : "Type d'avancement "Type avancement - Test 1" enregistré"
+		assertEquals("vérifie le message", "Type d'avancement \"Type avancement - Test 1\" enregistré",
+		typesDAvancementPage.lblConfirmationTest1.getText());
+		// Nom : Type avancement - Test 1		
+		assertEquals("vérifie le Nom","Type avancement - Test 1",typesDAvancementPage.lblNomTest1.getText());
+		// Activé : Case décochée et non modifiable
+		assertEquals("vérifie l'Activé est décochée",false,typesDAvancementPage.checkBoxActiveTest1.isSelected());
+		assertEquals("vérifie l'Activé est non modifiable",false,typesDAvancementPage.checkBoxActiveTest1.isEnabled());
+		// Prédéfini : Case décochée et non modifiable
+		assertEquals("vérifie Prédéfini",false,typesDAvancementPage.checkBoxPredefiniTest1.isSelected());
+		assertEquals("vérifie Prédéfini",false,typesDAvancementPage.checkBoxPredefiniTest1.isEnabled());
+		// Opérations : colonne contenant une icône "Modifier" et "Supprimer"
+		assertEquals("vérifie l'icône : Modifier",true,typesDAvancementPage.imgOperationsEditTest1.isDisplayed());
+		assertEquals("vérifie l'icône : Supprimer",true,typesDAvancementPage.imgOperationsDeleteTest1.isDisplayed());
 
 
 		// TODO : Etape 5 - Créer un type d'avancement - Accès au formulaire de création :
@@ -128,7 +129,7 @@ public class AVA_01_CreationTypeAvancementTest extends MasterTest {
 		// TODO : Etape 6 -Créer un type d'avancement - sans pourcentage (1/2) :
 		System.out.println("Etape 6 - Créer un type d'avancement - sans pourcentage (1/2) :");
 		// Remplie le champ Nom Unite et coche la case Pourcentage
-		typesDAvancementPage.remplirChampNomUniteCocherPercentage("Type avancement - Test 2");
+		typesDAvancementPage.fillFieldNomUniteCheckPercentage("Type avancement - Test 2");
 		// Vérifie que le champ Valuer Max est bien grisé
 		assertFalse(typesDAvancementPage.txtValeurMax.isEnabled());
 
@@ -141,7 +142,6 @@ public class AVA_01_CreationTypeAvancementTest extends MasterTest {
 		// Vérifie le message de confirmation 
 		String lblMessage = "Type d'avancement \"Type avancement - Test 2\" enregistré";
 		assertEquals("vérifie message de confirmation",lblMessage,typesDAvancementPage.lblConfirmationTest2.getText());
-
 		// Vérifie le titre pour la modification
 		String lblTitreModif = "Modifier Type d'avancement: Type avancement - Test 2";
 		assertEquals("Vérifie le titre de la modification",lblTitreModif,typesDAvancementPage.lblTitreModif.getText());
@@ -160,7 +160,7 @@ public class AVA_01_CreationTypeAvancementTest extends MasterTest {
 		// TODO : Etape 9 - Conformité du type d'avancement ajouté :
 		System.out.println("Etape 9 - Conformité du type d'avancement ajouté :");
 		// Vérifie Nom : Type avancement - Test 2
-		assertEquals("Vérifie l'élément : Nom","Type d'avancement \"Type avancement - Test 2\" enregistré",typesDAvancementPage.lblTabNom.getText());
+		assertEquals("Vérifie l'élément : Nom","Type avancement - Test 2",typesDAvancementPage.lblTabNomTest2.getText());
 		// Vérifie Activé : Case cochée et non modifiable
 		assertEquals("Vérifie l'élément est coché : Activite",true,typesDAvancementPage.lblTabActive.isSelected());
 		assertEquals("Vérifie l'élément est non modifiable : Activite",false,typesDAvancementPage.lblTabActive.isEnabled());
@@ -185,9 +185,9 @@ public class AVA_01_CreationTypeAvancementTest extends MasterTest {
 		String USER = "postgres";
 		String PASSWORD = "admin";
 		String querySelect = "select * from advance_type";
-		String queryRecupTest1 = "SELECT * FROM advance_type WHERE unit_name='Test 1'";
+		String queryRecupTest1 = "SELECT * FROM advance_type WHERE unit_name='Type avancement - Test 1'";
 		String queryRecupTest2 = "SELECT * FROM advance_type WHERE unit_name='Type avancement - Test 2'";
-		String queryDelete1 = "DELETE FROM advance_type WHERE unit_name='Test 1'";
+		String queryDelete1 = "DELETE FROM advance_type WHERE unit_name='Type avancement - Test 1'";
 		String queryDelete2 = "DELETE FROM advance_type WHERE unit_name='Type avancement - Test 2'";
 
 		//Load Postgre jdbc driver
